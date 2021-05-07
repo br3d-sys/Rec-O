cdc1c1a9a1fc43455deb9f109cb5eb3be31cf291
Branco <branco_937@hotmail.com>
2021-05-07 12:38:01 -0500

    actualizacion 12:37 7/5/2021

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/recfacial'/*'/home'*/;
+    protected $redirectTo = /*'/recfacial'*/'/home';
 
     /**
      * Create a new controller instance.
--- resources/views/admin/recognitions/req05.blade.php
+++ resources/views/admin/recognitions/req05.blade.php
@@ -1,35 +1,40 @@
 @extends('layouts.admin')
 @section('content')
-<div class="container bg-white p-5">
+<div class="container cards bg-white p-5">
   
-
-  
-<div class="row">
-  @foreach ($recognition as $item)
-  <div class="col-lg-6 p-2">
-    <div class="card">
-      <div class="row g-0 col-12">
-        <div class="col-md-6 d-flex align-items-center" >
-          <img width="100%" id="" class="imagen_mostrar" src="" alt="" attr-img ="{{$item->image}}">
-        </div>
-        <div class="col-md-6">
-          <div class="card-body">
-            <h5 class="card-title">{{DB::table('users')->where('id', $item->id_usuario)->first()->name}}</h5>
-            <p class="card-text">
-              Grado de Similitud: {{$item->similarity}}
-              <br>
-              Intento: {{$item->attempt}}
-              <br>
-              Fecha y Hora: {{$item->updated_at}}
-            </p>
+<div class="card-header">
+  <h4>REGISTRO DE IDENTIFICACIÓN</h4>
+</div>
+<div class="card-body">
+    <div class="row">
+    @foreach ($recognition as $item)
+    <div class="col-lg-6 p-2">
+      <div class="card">
+        <div class="row g-0 col-12">
+          <div class="col-md-6 d-flex align-items-center" >
+            <img width="100%" id="" class="imagen_mostrar" src="" alt="" attr-img ="{{$item->image}}">
+          </div>
+          <div class="col-md-6">
+            <div class="card-body">
+              <h5 class="card-title">{{DB::table('users')->where('id', $item->id_usuario)->first()->name}}</h5>
+              <p class="card-text">
+                Grado de Similitud: {{$item->similarity}}
+                <br>
+                Intento: {{$item->attempt}}
+                <br>
+                Fecha y Hora: {{$item->updated_at}}
+              </p>
+            </div>
           </div>
         </div>
       </div>
     </div>
+    
+    @endforeach
   </div>
-  
-  @endforeach
 </div>
+  
+
   
 </div>
 @endsection
